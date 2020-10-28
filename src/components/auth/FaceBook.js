8b0eb7d6f7d0b870902e7e0d52acd01de249deba
Abinash Mohapatra <abinash@expensebit.com>
2020-10-28 20:53:26 +0530

    changed

--- src/components/auth/FaceBook.js
+++ src/components/auth/FaceBook.js
@@ -3,22 +3,8 @@ import FacebookLogin from 'react-facebook-login';
 
 class FaceBook extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: false,
-            // userId: '',
-            // name: '',
-            // email: '',
-        }
-    }
-
     responseFacebook = facebookData => {
         this.props.userFacebookSignInRequest(facebookData);
-        // console.log(facebookData)
-        // setTimeout(() => {
-        //     window.location.replace('/')
-        // }, 1500);
     }
 
     componentClicked = () => {
@@ -26,26 +12,8 @@ class FaceBook extends Component {
     }
 
     render() {
-
-        // let fbContent;
-
-        // if (this.state.isLoggedIn) {
-        //     fbContent = null;
-        // } else {
-        //     fbContent = (
-        //         <FacebookLogin
-        //             appId="868067727065043"
-        //             autoLoad={true}
-        //             fields="name,email"
-        //             onClick={this.componentClicked}
-        //             callback={this.responseFacebook} />
-        //     );
-        // }
-
-
         return (
             <div>
-                {/* {fbContent} */}
                 <FacebookLogin
                     appId="FACEBOOK ID HERE"
                     autoLoad={true}
